4dcd8dbfc7cf14f5759c0119ac17af4a779d0291
Johanan Oppong Amoateng <88411614+JohananOppongAmoateng@users.noreply.github.com>
2024-12-27 19:06:55 +0000

    project setup

---
+++ home/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path, include
+from django.views.generic import TemplateView
+from .views import *
+
+from . import views
+
+
+app_name = "home"
+urlpatterns = [
+    path("", homepage, name="homepage"),
+    path("<int:year>/", views.home_for_year, name="home_for_year"),
+    path("hitcount/", include(("hitcount.urls", "hitcount"), namespace="hitcount")),
+]
